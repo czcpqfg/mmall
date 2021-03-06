8a26ec2442845e36c1a43e365c3b1617ca0a10b6
GYP <workbob@163.com>
2021-03-06 12:28:53 +0800

    20210306

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,43 @@
+package pojo;
+
+/**
+ * @author Penguin
+ * @create Date 2021/3/6  Time 12:27
+ */
+public class User {
+
+    String name;
+    int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
